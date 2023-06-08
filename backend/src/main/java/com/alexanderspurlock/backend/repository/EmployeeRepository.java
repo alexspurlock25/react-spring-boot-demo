6164c14bb801588d566dc65a7fa13c2ac747703e
Alexander Spurlock <alexanderspurlock@gmail.com>
2023-06-08 11:32:30 -0400

    annotation

--- backend/src/main/java/com/alexanderspurlock/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/alexanderspurlock/backend/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.alexanderspurlock.backend.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.alexanderspurlock.backend.model.Employee;
 
+@Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
     // auto generated crud operations
 }
