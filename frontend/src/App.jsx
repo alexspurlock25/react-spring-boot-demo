ccf2996a9ec24760daad8334b3c085f5bc91035d
Alexander Spurlock <alexanderspurlock@gmail.com>
2023-06-08 23:09:54 -0400

    styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -57,7 +57,7 @@ function App() {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1>Vite + React</h1>
+			<h1>Vite + React + Spring Boot</h1>
 			<div className="card">
 				<button onClick={addCounter}>Add Counter</button>
 				<div>
